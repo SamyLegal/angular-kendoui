de5566c838e1369317d616effd181b2f7bea44fa
legal-s <legal-s@mgdis.fr>
2017-11-17 18:08:37 +0100

    feat: Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
 
+import 'iframe-resizer/js/iframeResizer.contentWindow.js';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
